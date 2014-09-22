add925b8bea488f3fc47c3f45bbc36826955107e
imageservice <imageservice@carrot.com>
2014-09-22 17:24:04 +0800

    logging

--- imageserver/run.py
+++ imageserver/run.py
@@ -2,4 +2,4 @@ from imageserver.app import app
 
 
 if __name__ == "__main__":
-    app.run(use_debugger=True, use_reloader=True, debug=True, host='0.0.0.0', port=8000)
+    app.run(use_debugger=True, use_reloader=True, debug=True, host='0.0.0.0', port=8001)
